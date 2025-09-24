165ff76dae94853aa3f0223d82343f383fd4e588
arturbazza <agencia@fonzie.com.br>
2025-09-24 09:46:11 -0300

    Olá Flutter!

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
 
+main() {
+  runApp(new PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text('Olá Flutter'));
+  }
+}
