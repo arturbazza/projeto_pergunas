6cb9fb921e5f8f37124185873e2bd68ee52f3f25
arturbazza <agencia@fonzie.com.br>
2025-09-29 12:45:05 -0300

    Correção

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,20 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(PerguntaApp());
+void main() {
+  runApp(const PerguntaApp());
+}
 
 class PerguntaApp extends StatelessWidget {
+  const PerguntaApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
-      'Qual é a sua cor favorita?',
-      'Qual é o seu animal favorito?',
-    ];
-    
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Perguntas')),
-        body: Column(
-          children: <Widget>[
-            Text(perguntas[0]),
-            //RaisedButton(children: Text('Resposta 1'),
-            //onPressed: null,
-          ],
+        appBar: AppBar(
+          title: const Text('Perguntas'),
         ),
+        body: const Text('Olá Flutter!!!'),
       ),
     );
   }
