cfb524beb55989d76c0a0ed49bf56c0be3ee336f
arturbazza <agencia@fonzie.com.br>
2025-10-01 08:39:29 -0300

    Funcionando emuador com resposta da pergunta escolhida no terminal

--- lib/main.dart
+++ lib/main.dart
@@ -7,21 +7,54 @@ void main() {
 class PerguntaApp extends StatelessWidget {
   const PerguntaApp({super.key});
 
+  void responder() {
+    print('Pergunta respondida!');
+  }
+
   @override
   Widget build(BuildContext context) {
     final perguntas = [
       'Qual é a sua cor favorita?',
       'Qual é o seu animal favorito?',
     ];
-    
+
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Perguntas')),
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+        ),
         body: Column(
-          children: <Widget>[
+          children: [
             Text(perguntas[0]),
-            //RaisedButton(children: Text('Resposta 1'),
-            //onPressed: null,
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta 1'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('Resposta 2 foi selecionada!');
+              },
+              child: const Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('Resposta 3!!!'),
+              child: const Text('Resposta 3'),
+            ),
+            Text(perguntas[1]),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta 1'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('Resposta 2 foi selecionada!');
+              },
+              child: const Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('Resposta 3!!!'),
+              child: const Text('Resposta 3'),
+            ),
           ],
         ),
       ),
